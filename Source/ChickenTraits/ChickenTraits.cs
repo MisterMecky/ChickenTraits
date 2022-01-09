8f54b1653992425e930e8f0e9ce06271f14ecd2d
MisterMecky <62370007+MisterMecky@users.noreply.github.com>
2022-01-09 20:31:01 +0800

    bananBread

---
+++ Source/ChickenTraits/ChickenTraits.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ChickenTraits
+{
+    [StaticConstructorOnStartup]
+    public static class ChickenTraits
+    {
+        static ChickenTraits() // Banana Bread
+        {
+            Log.Message("Hello World!");
+        }
+    }
+}
