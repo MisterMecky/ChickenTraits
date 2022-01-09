2b1409b2384bab6f36ba01b1ef5f710a50184e3a
MisterMecky <62370007+MisterMecky@users.noreply.github.com>
2022-01-09 23:35:56 +0800

    adds stuff

---
+++ Source/ChickenTraits/ChickenDefOf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+
+namespace ChickenTraits
+{
+    [DefOf]
+    public static class ChickenDefOf
+    {
+        public static TraitDef Chicken_Germophobe;
+        public static TraitDef Chicken_Slapdash;
+        public static TraitDef Chicken_Bookworm;
+        public static TraitDef Chicken_Musician;
+        public static TraitDef Chicken_Unstable;
+    }
+}
---
+++ Source/ChickenTraits/Thoughts/Class1.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using ChickenTraits;
+using Verse;
+using Verse.AI;
+
+namespace ChickenTraits
+{
+    public class ThoughtWorker_GermophobeRoomCleanliness : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            return ThoughtState.Inactive;
+        }
+    }
+}
