292e53ce7e0ac01a279a9865d852e0104b374d15
MisterMecky <62370007+MisterMecky@users.noreply.github.com>
2022-01-10 11:26:13 +0800

    germophobia stuff

---
+++ Source/ChickenTraits/Thoughts/ThoughtWorker_GermophobeRoomCleanliness.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace ChickenTraits
+{
+    public class ThoughtWorker_GermophobeRoomCleanliness : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            Room room = p.GetRoom(RegionType.Set_All);
+            //Log.Message("Bop");
+            if (room==null)
+            {
+                return ThoughtState.Inactive;
+            }
+            int scoreStageIndex = RoomStatDefOf.Cleanliness.GetScoreStageIndex(room.GetStat(RoomStatDefOf.Cleanliness));
+            if (this.def.stages[scoreStageIndex] == null)
+            {
+                return ThoughtState.Inactive;
+            }
+            return ThoughtState.ActiveAtStage(scoreStageIndex);
+        }
+    }
+}
